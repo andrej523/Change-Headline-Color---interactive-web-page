296c065285d37f18a126accd5caa79f8eef577ff
Andrej <acucilovic23@gmail.com>
2017-09-26 17:27:49 -0400

    Project ready to be commited and shared with the world.

---
+++ app.js
@@ -0,0 +1,8 @@
+const myHeading = document.getElementsByTagName('h1')[0];
+const myButton = document.getElementById('myButton');
+const myTextInput = document.getElementById('myTextInput');
+
+myButton.addEventListener('click', () =>
+{
+    myHeading.style.color = myTextInput.value;
+});
